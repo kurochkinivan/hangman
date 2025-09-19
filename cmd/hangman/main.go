3b1f0bc0f7241383a0ac0e628564c78bfea9a197
kurochkinivan <vanya.kurochkin.23@mail.ru>
2025-09-19 12:22:19 +0700

    feat: refactor main function to remove stderr logging and streamline game initialization; delete unused stderr.log file

--- cmd/hangman/main.go
+++ cmd/hangman/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/internal/application"
@@ -9,16 +10,35 @@ import (
 )
 
 func main() {
-	f, err := os.OpenFile("stderr.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err == nil {
-		os.Stderr = f
+	wordsRepo := wordslist.NewRepository()
+
+	gameService, err := application.NewGameService(wordsRepo)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 
-	repo := wordslist.NewRepository()
+	switch len(os.Args[1:]) {
+	case 0:
+		h := terminal.NewGameHandler(gameService)
 
-	service := application.NewGameService(repo)
+		h.Start()
+	case 2:
+		word, guessed := os.Args[1], os.Args[2]
 
-	h := terminal.NewGameHandler(service)
-	
-	h.Start()
+		result, err := gameService.SimulateGame(word, guessed)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to start interactive mode: %v\n", err)
+			os.Exit(1)
+		}
+
+		if result.IsWon {
+			fmt.Printf("%s;POS\n", result.WordMask)
+		} else {
+			fmt.Printf("%s;NEG\n", result.WordMask)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid number of arguments\n")
+		os.Exit(1)
+	}
 }
