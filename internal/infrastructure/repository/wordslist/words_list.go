cb0d4cdcb713efcd3721275f56bb6e623d71005a
kurochkinivan <vanya.kurochkin.23@mail.ru>
2025-09-19 12:21:46 +0700

    feat: update words repository to use WordEntry struct for enhanced word and hint storage; modify RandomWord method to return structured word data

--- internal/infrastructure/repository/wordslist/words_list.go
+++ internal/infrastructure/repository/wordslist/words_list.go
@@ -4,37 +4,87 @@ import (
 	"errors"
 	"math/rand/v2"
 
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/internal/application/dto"
 	ent "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/internal/domain/entities"
 )
 
-type Repository struct {
-	wordsMap map[ent.Category]map[ent.Level][]string
+type WordEntry struct {
+	Value string
+	Hint  string
 }
 
-// IDEA: Можно использовать [2]int для хранения подсказки
+type Repository struct {
+	wordsMap map[ent.Category]map[ent.Level][]WordEntry
+}
+
 func NewRepository() *Repository {
 	return &Repository{
-		wordsMap: map[ent.Category]map[ent.Level][]string{
+		wordsMap: map[ent.Category]map[ent.Level][]WordEntry{
 			ent.CategoryAnimals: {
-				ent.LevelEasy:   {"cat", "dog", "cow", "pig"},
-				ent.LevelMedium: {"lion", "tiger", "zebra", "giraffe"},
-				ent.LevelHard:   {"elephant", "rhinoceros", "hippopotamus", "kangaroo"},
+				ent.LevelEasy: {
+					{"cat", "domestic pet"},
+					{"dog", "man's best friend"},
+					{"cow", "gives milk"},
+					{"pig", "lives on a farm"},
+				},
+				ent.LevelMedium: {
+					{"lion", "king of the jungle"},
+					{"tiger", "striped predator"},
+					{"zebra", "striped horse"},
+					{"giraffe", "long neck"},
+				},
+				ent.LevelHard: {
+					{"elephant", "largest land animal"},
+					{"rhinoceros", "has a horn"},
+					{"hippopotamus", "lives near rivers"},
+					{"kangaroo", "jumps and has a pouch"},
+				},
 			},
 			ent.CategoryFruitsVegetables: {
-				ent.LevelEasy:   {"apple", "pear", "plum", "carrot"},
-				ent.LevelMedium: {"banana", "orange", "pepper", "potato"},
-				ent.LevelHard:   {"pomegranate", "asparagus", "artichoke", "cucumber"},
+				ent.LevelEasy: {
+					{"apple", "keeps the doctor away"},
+					{"pear", "bell-shaped fruit"},
+					{"plum", "small purple fruit"},
+					{"carrot", "orange vegetable"},
+				},
+				ent.LevelMedium: {
+					{"banana", "yellow curved fruit"},
+					{"orange", "citrus fruit"},
+					{"pepper", "can be sweet or spicy"},
+					{"potato", "grows underground"},
+				},
+				ent.LevelHard: {
+					{"pomegranate", "many red seeds inside"},
+					{"asparagus", "green spring vegetable"},
+					{"artichoke", "edible flower bud"},
+					{"cucumber", "green salad vegetable"},
+				},
 			},
 			ent.CategoryCountries: {
-				ent.LevelEasy:   {"egypt", "india", "italy", "china"},
-				ent.LevelMedium: {"canada", "germany", "brazil", "japan"},
-				ent.LevelHard:   {"australia", "kazakhstan", "singapore", "switzerland"},
+				ent.LevelEasy: {
+					{"egypt", "pyramids"},
+					{"india", "Taj Mahal"},
+					{"italy", "pizza & pasta"},
+					{"china", "Great Wall"},
+				},
+				ent.LevelMedium: {
+					{"canada", "maple leaf"},
+					{"germany", "beer & sausages"},
+					{"brazil", "Rio carnival"},
+					{"japan", "samurai & sushi"},
+				},
+				ent.LevelHard: {
+					{"australia", "kangaroos and koalas"},
+					{"kazakhstan", "largest landlocked country"},
+					{"singapore", "city-state in Asia"},
+					{"switzerland", "Alps and chocolate"},
+				},
 			},
 		},
 	}
 }
 
-func (r *Repository) RandomWord(config *ent.GameConfig) (*ent.Word, error) {
+func (r *Repository) RandomWord(config *ent.GameConfig) (*dto.Word, error) {
 	categoryWords, ok := r.wordsMap[config.Category()]
 	if !ok {
 		return nil, errors.New("category not found")
@@ -49,7 +99,7 @@ func (r *Repository) RandomWord(config *ent.GameConfig) (*ent.Word, error) {
 		return nil, errors.New("no words are available for this category and level")
 	}
 
-	value := levelWords[rand.IntN(len(levelWords))]
+	wordEntry := levelWords[rand.IntN(len(levelWords))]
 
-	return ent.NewWord(value, config.Level(), config.Category()), nil
+	return dto.NewWord(wordEntry.Value, wordEntry.Hint), nil
 }
