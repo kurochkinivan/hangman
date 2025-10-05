61f2d975fc3b803ba58dd2f61628e1cd37afeed9
kurochkinivan <vanya.kurochkin.23@mail.ru>
2025-10-06 00:37:54 +0700

    feat: refactor main function to improve game initialization and error handling

--- cmd/hangman/main.go
+++ cmd/hangman/main.go
@@ -4,39 +4,34 @@ import (
 	"fmt"
 	"os"
 
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/internal/application"
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/internal/infrastructure/repository/wordslist"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/internal/infrastructure/simulate"
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/internal/infrastructure/terminal"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw1-hangman/pkg/random"
+)
+
+const (
+	pathToWordsYAML = "../../words/words.yaml"
 )
 
 func main() {
-	wordsRepo := wordslist.NewRepository()
-
-	gameService, err := application.NewGameService(wordsRepo)
+	wl, err := wordslist.LoadWordsListFromYAML(pathToWordsYAML)
 	if err != nil {
-		fmt.Fprint(os.Stderr, err.Error())
+		fmt.Fprintf(os.Stderr, "failed to load repository from yaml: %v\n", err.Error())
 		os.Exit(1)
 	}
+	randSelector := random.New[wordslist.Word]()
+	wordsRepo := wordslist.NewRepository(wl, randSelector)
 
 	switch len(os.Args[1:]) {
 	case 0:
-		h := terminal.NewGameHandler(gameService)
-
-		h.Start()
+		gh := terminal.NewGameHandler(wordsRepo, os.Stdin)
+		gh.Start()
 	case 2:
 		word, guessed := os.Args[1], os.Args[2]
 
-		result, err := gameService.SimulateGame(word, guessed)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to start interactive mode: %v\n", err)
-			os.Exit(1)
-		}
-
-		if result.IsWon {
-			fmt.Printf("%s;POS\n", result.WordMask)
-		} else {
-			fmt.Printf("%s;NEG\n", result.WordMask)
-		}
+		gh := simulate.NewGameHandler(word, guessed)
+		gh.Start()
 	default:
 		fmt.Fprintf(os.Stderr, "Invalid number of arguments\n")
 		os.Exit(1)
